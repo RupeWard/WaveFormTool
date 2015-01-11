3b17c143d19cb50d9518ddafc60fe1d1f8d1f271
Rupe-Parlour <rupe@rupertward.co.uk>
2015-01-11 17:37:59 +0000

    Fix screen size issue on device - work out root's active width from its height and physical screen dims

--- Unity/WaveFormTool/Assets/Scripts/GUI/GuiManager.cs
+++ Unity/WaveFormTool/Assets/Scripts/GUI/GuiManager.cs
@@ -5,9 +5,24 @@ using System.Collections.Generic;
 public class GuiManager : SingletonApplicationLifetime< GuiManager >
 {
 	public GraphPanel waveGraphPanel;
+	public UIRoot myRoot;
 
 	public Transform mainMenuBarHeightProvider;
 	public GameObject mainMenuBar;
+
+	private Vector2 screenDims_;
+	public Vector2 ScreenDims
+	{
+		get { return screenDims_; }
+	}
+	public float ScreenWidth 
+	{
+		get { return screenDims_.x; } 
+	}
+	public float ScreenHeight 
+	{
+		get { return screenDims_.y; } 
+	}
 
 	private float margin_ = 10f;
 	public float Margin
@@ -17,13 +32,20 @@ public class GuiManager : SingletonApplicationLifetime< GuiManager >
 
 	private List< GameObject > popUps_  = new List< GameObject >();
 
+	protected override void PostAwake()
+	{
+		screenDims_.y = myRoot.activeHeight;
+		screenDims_.x = myRoot.activeHeight * Screen.width / Screen.height;
+		Debug.Log ("GuiManager.Awake: Screen is " + Screen.width + " x " + Screen.height
+		           + " so root dims = "+screenDims_);
+	}
+
 	public void Start()
 	{
-		Debug.Log ("GuiManager.Awake: Screen is " + Screen.width + " x " + Screen.height);
 		mainMenuBar.transform.SetLocalXYPosition(0f, -0.5f * mainMenuBarHeightProvider.localScale.y - margin_);
 		float graphTop = -1f *(mainMenuBarHeightProvider.localScale.y + 2 * margin_);
-		float graphBottom = -1f * (Screen.height - 50f);
-		waveGraphPanel.init ( new Vector2 (Screen.width - 2*margin_ ,graphTop - graphBottom), 
+		float graphBottom = -1f * (ScreenHeight - 50f);
+		waveGraphPanel.init ( new Vector2 (ScreenWidth - 2*margin_ ,graphTop - graphBottom), 
 		                     new Vector2 (0f ,0.5f * (graphTop+graphBottom)) );
 
 		float popUpTop = graphTop - 5;
@@ -56,4 +78,22 @@ public class GuiManager : SingletonApplicationLifetime< GuiManager >
 			}
 		}
 	}
+
+	public void Update()
+	{
+		if (Application.platform == RuntimePlatform.Android)
+		{
+			if (Input.GetKey(KeyCode.Escape))
+			{
+				Application.Quit();
+				return;
+			}
+			if (Input.GetKey(KeyCode.Menu))
+			{
+				return;
+			}
+		}
+
+	}
+
 }
