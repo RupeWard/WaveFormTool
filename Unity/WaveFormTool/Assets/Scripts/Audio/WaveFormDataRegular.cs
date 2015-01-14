34649d1031e39275b71e52a25b219351c88d00bb
Rupe-Parlour <rupe@rupertward.co.uk>
2015-01-14 00:11:05 +0000

    Added DD interface to waveformdataregular

--- Unity/WaveFormTool/Assets/Scripts/Audio/WaveFormDataRegular.cs
+++ Unity/WaveFormTool/Assets/Scripts/Audio/WaveFormDataRegular.cs
@@ -5,7 +5,7 @@
 using System;
 using UnityEngine;
 
-public class WaveFormDataRegular : IWaveFormProvider
+public class WaveFormDataRegular : IWaveFormProvider, IDebugDescribable
 {
 	private string waveformName_;
 
